02d463dc9d136bd950de1f92a0e3f142134ab04d
junrui <754055908@qq.com>
2018-05-15 17:45:00 +0800

    add live each evaluation

--- examples/eval_each_live.py
+++ examples/eval_each_live.py
@@ -2,44 +2,32 @@ import numpy as np
 from scipy import stats
 import face_iqa
 from skimage import io
+import os
 
 tp = 'FT_each'
 
-
-model_path = '/data/junrui/github/transIQA/model/ft12/tid2013_mse_423_0.8354_0.8103.pth.tar'
+model_path = '/data/junrui/github/transIQA/model/ft12/live_mse_405_0.9725_0.9674.pth.tar'
 fi = face_iqa.FaceIQA(enable_cuda=True, model_path=model_path)
 
-res_dir = './log/results/tid2013/'
+res_dir = './log/results/live/'
 
 srocc_file = open(res_dir + tp + '_srocc' + '.txt', "w")
 lcc_file = open(res_dir + tp + '_lcc' + '.txt', "w")
 
-test_file = './data/tid2013_generator/' + 'ft_tid2013_test.txt'
+test_file = './data/live_generator/' + 'ft_live_test.txt'
 filename = [line.rstrip('\n') for line in open(test_file)]
 
 
-for tp in range(1, 25):
-    
+for tp in ['gblur', 'wn', 'jpeg', 'jp2k', 'fastfading']:
+
     roidb = []
-    scores =[]
-    tmp_labels=[]
-    tmp_dir=[]
+    scores = []
     for i in filename:
-        tmp_dir.append(i.split()[0])
-        tmp_labels.append(float(i.split()[1]))
-    tmp_labels = np.asarray(tmp_labels)
-    #print scores
-    
-    for i in range(len(tmp_labels)):
-        #print tmp_dir[i][-8:-6]
-        if int(tmp_dir[i][-8:-6]) == int(tp):
-            
-            roidb.append(tmp_dir[i])
-            scores.append(tmp_labels[i]) 
-            
+        if os.path.split(os.path.split(i.split()[0])[0])[1] == tp:
+            roidb.append(i.split()[0])
+            scores.append(float(i.split()[1]))
     scores = np.asarray(scores)
-    
-    
+
     Num_Image = len(scores)
     # feat = np.zeros([Num_Image,Num_Patch])
     pre = np.zeros(Num_Image)
@@ -47,9 +35,9 @@ for tp in range(1, 25):
 
     for i in range(Num_Image):
         directory = roidb[i]
-        #im = np.asarray(cv2.imread(directory))
+        # im = np.asarray(cv2.imread(directory))
         im = io.imread(directory)
-        #for j in range(Num_Patch):
+        # for j in range(Num_Patch):
         #    x =  im.shape[0]
         #    y = im.shape[1]
         #    x_p = np.random.randint(x-224,size=1)[0]
@@ -59,23 +47,21 @@ for tp in range(1, 25):
         #    out = net.forward_all(data=np.asarray([temp]))
         #    feat[i,j] = out[ft][0]
         #    pre[i] += out[ft][0]
-        #pre[i] /= Num_Patch
-        #med [i] = np.median(feat[i,:])
+        # pre[i] /= Num_Patch
+        # med [i] = np.median(feat[i,:])
         pre[i] = fi.get_score(im)
     srocc = stats.spearmanr(pre, scores)[0]
     lcc = stats.pearsonr(pre, scores)[0]
     print(str(tp) + '%   LCC of mean: {}'.format(lcc))
     print(str(tp) + '% SROCC of mean: {}'.format(srocc))
-    
-    srocc_file.write('%6.3f\n' % (srocc))
-    lcc_file.write('%6.3f\n' % (lcc))
-    
-    #srocc = stats.spearmanr(med,scores)[0]
-    #lcc = stats.pearsonr(med,scores)[0]
-    #print str(tp) +'%   LCC of median: {}'.format(lcc)
-    #print str(tp) + '% SROCC of median: {}'.format(srocc)
 
+    srocc_file.write('%s: %6.3f\n' % (tp, srocc))
+    lcc_file.write('%s: %6.3f\n' % (tp, lcc))
+
+    # srocc = stats.spearmanr(med,scores)[0]
+    # lcc = stats.pearsonr(med,scores)[0]
+    # print str(tp) +'%   LCC of median: {}'.format(lcc)
+    # print str(tp) + '% SROCC of median: {}'.format(srocc)
 
 srocc_file.close()
 lcc_file.close()
-
